23056f3207a80ed9c4d4270a9c8c01a6a2bd1bfc
simonr0204 <simon.roberts@fuel.sh>
2022-09-27 15:58:03 -0400

    factor setup out of loop

--- contract-message-executor/src/main.rs
+++ contract-message-executor/src/main.rs
@@ -8,15 +8,6 @@ use std::{thread, time};
 
 #[tokio::main]
 async fn main() {
-    // This will check for new messages to relay every 10 seconds
-    let period = time::Duration::new(10, 0);
-    loop {
-        relay_messages().await;
-        thread::sleep(period);
-    }
-}
-
-async fn relay_messages() {
     // Connect to provider
     let provider = Provider::connect("node-beta-1.fuel.network").await.unwrap();
 
@@ -24,18 +15,29 @@ async fn relay_messages() {
     let phrase = "oblige salon price punch saddle immune slogan rare snap desert retire surprise";
     let wallet = WalletUnlocked::new_from_mnemonic_phrase(phrase, Some(provider.clone())).unwrap();
 
-    // Get predicate bytecode root
+    // Get predicate bytecode root address to check for messages at
     let (_, predicate_root) = utils::get_contract_message_predicate().await;
+    let predicate_address = Bech32Address::from(predicate_root);
 
-    // Get unspend messages belonging to the predicate bytecode root
-    let unspent_messages = provider
-        .get_messages(&Bech32Address::from(predicate_root))
-        .await
-        .unwrap();
+    // This will check for new messages every 10 seconds, and relay them.
+    let period = time::Duration::new(10, 0);
+    loop {
+        relay_messages(provider.clone(), &wallet, &predicate_address).await;
+        thread::sleep(period);
+    }
+}
+
+async fn relay_messages(
+    provider: Provider,
+    wallet: &WalletUnlocked,
+    predicate_address: &Bech32Address,
+) {
+    // Get unspent messages belonging to the predicate address
+    let unspent_messages = provider.get_messages(predicate_address).await.unwrap();
 
     // Relay the messages
     for message in unspent_messages {
         // TO DO: `message` is a `fuels::client::schema::message::Message`, but we need a `fuels::tx::Input`
-        //utils::relay_message_to_contract(&wallet, message);
+        //utils::relay_message_to_contract(wallet, message);
     }
 }
