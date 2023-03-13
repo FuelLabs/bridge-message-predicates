05193a0cf88ec7a7b85bda1a23870479b1c887fc
PixelCircuits <circuitpixels@gmail.com>
2023-03-12 21:37:11 -0400

    removed possible word alignment issues

--- contract-message-predicate/src/script_asm.rs
+++ contract-message-predicate/src/script_asm.rs
@@ -5,8 +5,7 @@ const PROCESS_MESSAGE_FUNCTION_SIGNATURE: &str = "process_message(u8)";
 const GTF_MSG_DATA: u16 = 0x11D;
 const GTF_MSG_AMOUNT: u16 = 0x117;
 
-const INSTR_PER_WORD: u16 = 2;
-const BYTES_PER_WORD: u16 = 8;
+const BYTES_PER_INSTR: u16 = 4;
 
 // Gets the bytecode for the message-to-contract script
 pub fn bytecode() -> Vec<u8> {
@@ -23,9 +22,10 @@ pub fn bytecode() -> Vec<u8> {
     const MEMORY_START_PTR: u8 = 0x10;
     const ASSET_ID_PTR: u8 = MEMORY_START_PTR;
     const CALL_DATA_PTR: u8 = 0x11;
-    const CONTRACT_ADDR_PTR: u8 = 0x12;
-    const FN_SELECTOR: u8 = 0x13;
-    const MSG_AMOUNT: u8 = 0x14;
+    const CALL_DATA_FN_SEL_PTR: u8 = 0x12;
+    const CONTRACT_ADDR_PTR: u8 = 0x13;
+    const FN_SELECTOR_PTR: u8 = 0x14;
+    const MSG_AMOUNT: u8 = 0x15;
 
     /* The following assembly code is intended to do the following:
      *  -Call the function `process_message` on the contract with ID that matches
@@ -36,24 +36,24 @@ pub fn bytecode() -> Vec<u8> {
         //extend stack for contract call data
         op::move_(MEMORY_START_PTR, STACK_PTR), //MEMORY_START_PTR = stack pointer
         op::cfei(32 + 32 + 8 + 8), //extends current call frame stack by 32+32+8+8 bytes [base asset id, contract id, param1, param2]
-        //prep call parameters
         op::addi(CALL_DATA_PTR, MEMORY_START_PTR, 32), //CALL_DATA_PTR = MEMORY_START_PTR + 32bytes [memory start pointer + 32]
-        op::lw(FN_SELECTOR, INSTR_START, 10 / INSTR_PER_WORD), //FN_SELECTOR = function selector at end of program [00000000 9532D7AE]
+        op::addi(CALL_DATA_FN_SEL_PTR, CALL_DATA_PTR, 32 + 4), //CALL_DATA_FN_SEL_PTR = CALL_DATA_PTR + 32bytes + 4bytes [call data start pointer + 32 + 4]
+        //prep call parameters
         op::gtf(MSG_AMOUNT, ZERO, GTF_MSG_AMOUNT), //MSG_AMOUNT = amount value of message from input[0]
         op::gtf(CONTRACT_ADDR_PTR, ZERO, GTF_MSG_DATA), //CONTRACT_ADDR_PTR = memory location of the message data from input[0]
+        op::addi(FN_SELECTOR_PTR, INSTR_START, 11 * BYTES_PER_INSTR), //FN_SELECTOR_PTR = function selector at end of program
         op::mcpi(CALL_DATA_PTR, CONTRACT_ADDR_PTR, 32), //32 bytes at CALL_DATA_PTR = the 32 bytes at CONTRACT_ADDR_PTR
-        op::sw(CALL_DATA_PTR, FN_SELECTOR, 32 / BYTES_PER_WORD), //the 8bytes at CALL_DATA_PTR + 32 = FN_SELECTOR [00000000 9532D7AE]
+        op::mcpi(CALL_DATA_FN_SEL_PTR, FN_SELECTOR_PTR, 4), //4 bytes at CALL_DATA_FN_SEL_PTR = the 4 bytes at FN_SELECTOR_PTR
         //make contract call
         op::call(CALL_DATA_PTR, MSG_AMOUNT, ASSET_ID_PTR, CGAS),
         op::ret(ZERO),
-        //word aligned referenced data (function selector)
-        //00000000 00000000
+        //referenced data (function selector)
+        //00000000
     ]
     .into_iter()
     .collect();
 
     //add referenced data (function selector)
-    script.append(&mut vec![0x00, 0x00, 0x00, 0x00]);
     script.append(&mut fn_sel_hash[0..4].to_vec());
     script
 }
