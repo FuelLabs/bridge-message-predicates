c06fd6da3c83683d5a677959a7b225d3b07c4110
PixelCircuits <circuitpixels@gmail.com>
2023-03-12 21:22:59 -0400

    removed setting a zero value

--- contract-message-predicate/src/script_asm.rs
+++ contract-message-predicate/src/script_asm.rs
@@ -38,16 +38,14 @@ pub fn bytecode() -> Vec<u8> {
         op::cfei(32 + 32 + 8 + 8), //extends current call frame stack by 32+32+8+8 bytes [base asset id, contract id, param1, param2]
         //prep call parameters
         op::addi(CALL_DATA_PTR, MEMORY_START_PTR, 32), //CALL_DATA_PTR = MEMORY_START_PTR + 32bytes [memory start pointer + 32]
-        op::lw(FN_SELECTOR, INSTR_START, 12 / INSTR_PER_WORD), //FN_SELECTOR = function selector at end of program [00000000 9532D7AE]
+        op::lw(FN_SELECTOR, INSTR_START, 10 / INSTR_PER_WORD), //FN_SELECTOR = function selector at end of program [00000000 9532D7AE]
         op::gtf(MSG_AMOUNT, ZERO, GTF_MSG_AMOUNT), //MSG_AMOUNT = amount value of message from input[0]
         op::gtf(CONTRACT_ADDR_PTR, ZERO, GTF_MSG_DATA), //CONTRACT_ADDR_PTR = memory location of the message data from input[0]
         op::mcpi(CALL_DATA_PTR, CONTRACT_ADDR_PTR, 32), //32 bytes at CALL_DATA_PTR = the 32 bytes at CONTRACT_ADDR_PTR
         op::sw(CALL_DATA_PTR, FN_SELECTOR, 32 / BYTES_PER_WORD), //the 8bytes at CALL_DATA_PTR + 32 = FN_SELECTOR [00000000 9532D7AE]
-        op::sw(CALL_DATA_PTR, ZERO, (32 + 8) / BYTES_PER_WORD), //the 8bytes at CALL_DATA_PTR + 32 + 8 = 0
         //make contract call
         op::call(CALL_DATA_PTR, MSG_AMOUNT, ASSET_ID_PTR, CGAS),
         op::ret(ZERO),
-        op::noop(),
         //word aligned referenced data (function selector)
         //00000000 00000000
     ]
