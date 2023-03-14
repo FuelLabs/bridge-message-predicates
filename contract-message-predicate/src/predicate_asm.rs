48c043aa20d8af9065f7ceabfa2d03e8df48ba70
PixelCircuits <circuitpixels@gmail.com>
2023-03-13 22:10:46 -0400

    imporved assembly readability

--- contract-message-predicate/src/predicate_asm.rs
+++ contract-message-predicate/src/predicate_asm.rs
@@ -6,26 +6,30 @@ const GTF_SCRIPT_INPUTS_COUNT: u16 = 0x007;
 const GTF_INPUT_TYPE: u16 = 0x101;
 const GTF_MSG_DATA_LEN: u16 = 0x11A;
 
-const INPUT_MESSAGE_TYPE: u16 = 2;
+const INPUT_MESSAGE_TYPE: u32 = 2;
 const BYTES_PER_INSTR: u16 = 4;
 
 // Gets the bytecode for the message-to-contract predicate
 pub fn bytecode() -> Vec<u8> {
     //register names
-    const ZERO: RegId = RegId::ZERO;
-    const ONE: RegId = RegId::ONE;
-    const STACK_PTR: RegId = RegId::SP;
-    const INSTR_START: RegId = RegId::IS;
-    const SCRIPT_HASH_PTR: u8 = 0x10;
-    const SCRIPT_PTR: u8 = 0x11;
-    const SCRIPT_LEN: u8 = 0x12;
-    const EXPECTED_HASH_PTR: u8 = 0x13;
-    const COMPARE_RESULT: u8 = 0x14;
-    const VAL_32: u8 = 0x16;
-    const INPUT_INDEX: u8 = 0x17;
-    const INPUT_TYPE: u8 = 0x18;
-    const INPUT_MSG_DATA_LEN: u8 = 0x19;
-    const EXPECTED_INPUT_TYPE: u8 = 0x20;
+    const REG_HASH_PTR: u8 = 0x10;
+    const REG_SCRIPT_PTR: u8 = 0x11;
+    const REG_SCRIPT_LEN: u8 = 0x12;
+    const REG_EXPECTED_HASH_PTR: u8 = 0x13;
+    const REG_RESULT: u8 = 0x14;
+    const REG_VAL_32: u8 = 0x16;
+    const REG_INPUT_INDEX: u8 = 0x17;
+    const REG_INPUT_TYPE: u8 = 0x18;
+    const REG_INPUT_MSG_DATA_LEN: u8 = 0x19;
+    const REG_EXPECTED_INPUT_TYPE: u8 = 0x1a;
+
+    //instruction jump points
+    const JMP_LOOP_START: u16 = 11;
+    const JMP_SKIP_DATA_CHECK: u16 = 16;
+    const JMP_PREDICATE_FAILURE: u16 = 18;
+
+    //referenced data start pointer
+    const REF_DATA_START_PTR: u16 = 19 * BYTES_PER_INSTR;
 
     /* The following assembly code is intended to do the following:
      *  -Verify that the script bytecode hash for the transaction matches that of
@@ -37,33 +41,33 @@ pub fn bytecode() -> Vec<u8> {
      */
     let mut predicate: Vec<u8> = vec![
         //extend stack for storing script hash
-        op::move_(SCRIPT_HASH_PTR, STACK_PTR), //SCRIPT_HASH_PTR = stack pointer
-        op::cfei(32),                          //extends current call frame stack by 32 bytes
+        op::move_(REG_HASH_PTR, RegId::SP), //REG_HASH_PTR = stack pointer
+        op::cfei(32),                       //extends current call frame stack by 32 bytes
         //compute script hash
-        op::gtf(SCRIPT_PTR, ZERO, GTF_SCRIPT), //SCRIPT_PTR = script data address
-        op::gtf(SCRIPT_LEN, ZERO, GTF_SCRIPT_LEN), //SCRIPT_LEN = script data length
-        op::s256(SCRIPT_HASH_PTR, SCRIPT_PTR, SCRIPT_LEN), //32bytes at SCRIPT_HASH_PTR = hash of the script
+        op::gtf(REG_SCRIPT_PTR, RegId::ZERO, GTF_SCRIPT), //REG_SCRIPT_PTR = script data address
+        op::gtf(REG_SCRIPT_LEN, RegId::ZERO, GTF_SCRIPT_LEN), //REG_SCRIPT_LEN = script data length
+        op::s256(REG_HASH_PTR, REG_SCRIPT_PTR, REG_SCRIPT_LEN), //32bytes at SCRIPT_HASH_PTR = hash of the script
         //compare hash with expected
-        op::addi(EXPECTED_HASH_PTR, INSTR_START, 19 * BYTES_PER_INSTR), //EXPECTED_HASH_PTR = address of reference data at end of program
-        op::movi(VAL_32, 32),                                           //VAL_32 = 32
-        op::meq(COMPARE_RESULT, EXPECTED_HASH_PTR, SCRIPT_HASH_PTR, VAL_32), //COMPARE_RESULT = if the 32bytes at SCRIPT_HASH_PTR equals the 32bytes at EXPECTED_HASH_PTR
-        op::jnei(COMPARE_RESULT, ONE, 18), //jumps to PREDICATE_FAILURE if COMPARE_RESULT is not 1
+        op::addi(REG_EXPECTED_HASH_PTR, RegId::IS, REF_DATA_START_PTR), //REG_EXPECTED_HASH_PTR = address of reference data at end of program
+        op::movi(REG_VAL_32, 32),                                       //REG_VAL_32 = 32
+        op::meq(REG_RESULT, REG_EXPECTED_HASH_PTR, REG_HASH_PTR, REG_VAL_32), //REG_RESULT = if the 32bytes at REG_HASH_PTR equals the 32bytes at REG_EXPECTED_HASH_PTR
+        op::jnei(REG_RESULT, RegId::ONE, JMP_PREDICATE_FAILURE), //jumps to PREDICATE_FAILURE if REG_RESULT is not 1
         //confirm that no other messages with data are included
-        op::gtf(INPUT_INDEX, ZERO, GTF_SCRIPT_INPUTS_COUNT), //INPUT_INDEX = the number of inputs in the script
-        op::addi(EXPECTED_INPUT_TYPE, ZERO, INPUT_MESSAGE_TYPE), //EXPECTED_INPUT_TYPE = INPUT_MESSAGE_TYPE
+        op::gtf(REG_INPUT_INDEX, RegId::ZERO, GTF_SCRIPT_INPUTS_COUNT), //REG_INPUT_INDEX = the number of inputs in the script
+        op::movi(REG_EXPECTED_INPUT_TYPE, INPUT_MESSAGE_TYPE), //REG_EXPECTED_INPUT_TYPE = REG_INPUT_MESSAGE_TYPE
         //LOOP_START:
-        op::subi(INPUT_INDEX, INPUT_INDEX, 1), //INPUT_INDEX = INPUT_INDEX - 1
+        op::subi(REG_INPUT_INDEX, REG_INPUT_INDEX, 1), //REG_INPUT_INDEX = REG_INPUT_INDEX - 1
         //check if the input is a message input
-        op::gtf(INPUT_TYPE, INPUT_INDEX, GTF_INPUT_TYPE), //INPUT_TYPE = the type of input for input[INPUT_INDEX]
-        op::jnei(INPUT_TYPE, EXPECTED_INPUT_TYPE, 16), //skips to SKIP_DATA_CHECK if INPUT_TYPE does not equal EXPECTED_INPUT_TYPE
+        op::gtf(REG_INPUT_TYPE, REG_INPUT_INDEX, GTF_INPUT_TYPE), //REG_INPUT_TYPE = the type of input for input[INPUT_INDEX]
+        op::jnei(REG_INPUT_TYPE, REG_EXPECTED_INPUT_TYPE, JMP_SKIP_DATA_CHECK), //skips to SKIP_DATA_CHECK if REG_INPUT_TYPE does not equal REG_EXPECTED_INPUT_TYPE
         //check it the input message has data
-        op::gtf(INPUT_MSG_DATA_LEN, INPUT_INDEX, GTF_MSG_DATA_LEN), //INPUT_MSG_DATA_LEN = the data length of input[INPUT_INDEX]
-        op::jnei(INPUT_MSG_DATA_LEN, ZERO, 18), //jumps to PREDICATE_FAILURE if INPUT_MSG_DATA_LEN does not equal 0
+        op::gtf(REG_INPUT_MSG_DATA_LEN, REG_INPUT_INDEX, GTF_MSG_DATA_LEN), //REG_INPUT_MSG_DATA_LEN = the data length of input[INPUT_INDEX]
+        op::jnei(REG_INPUT_MSG_DATA_LEN, RegId::ZERO, JMP_PREDICATE_FAILURE), //jumps to PREDICATE_FAILURE if REG_INPUT_MSG_DATA_LEN does not equal 0
         //SKIP_DATA_CHECK:
-        op::jnei(INPUT_INDEX, ONE, 11), //jumps back to LOOP_START if INPUT_INDEX does not equal 1
-        op::ret(ONE),
+        op::jnei(REG_INPUT_INDEX, RegId::ONE, JMP_LOOP_START), //jumps back to LOOP_START if REG_INPUT_INDEX does not equal 1
+        op::ret(RegId::ONE),
         //PREDICATE_FAILURE:
-        op::ret(ZERO),
+        op::ret(RegId::ZERO),
         //referenced data (expected script hash)
         //00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000
     ]
--- contract-message-predicate/src/script_asm.rs
+++ contract-message-predicate/src/script_asm.rs
@@ -3,7 +3,7 @@ use sha2::{Digest, Sha256};
 
 const PROCESS_MESSAGE_FUNCTION_SIGNATURE: &str = "process_message(u8)";
 const GTF_MSG_DATA: u16 = 0x11D;
-const GTF_MSG_AMOUNT: u16 = 0x117;
+const GTF_REG_MSG_AMOUNT: u16 = 0x117;
 
 const BYTES_PER_INSTR: u16 = 4;
 
@@ -15,38 +15,39 @@ pub fn bytecode() -> Vec<u8> {
     let fn_sel_hash: [u8; 32] = fn_sel_hasher.finalize().into();
 
     //register names
-    const ZERO: RegId = RegId::ZERO;
-    const STACK_PTR: RegId = RegId::SP;
-    const INSTR_START: RegId = RegId::IS;
-    const CGAS: RegId = RegId::CGAS;
-    const MEMORY_START_PTR: u8 = 0x10;
-    const ASSET_ID_PTR: u8 = MEMORY_START_PTR;
-    const CALL_DATA_PTR: u8 = 0x11;
-    const CALL_DATA_FN_SEL_PTR: u8 = 0x12;
-    const CONTRACT_ADDR_PTR: u8 = 0x13;
-    const FN_SELECTOR_PTR: u8 = 0x14;
-    const MSG_AMOUNT: u8 = 0x15;
+    const REG_MEMORY_START_PTR: u8 = 0x10;
+    const REG_ASSET_PTR: u8 = REG_MEMORY_START_PTR;
+    const REG_DATA_PTR: u8 = 0x11;
+    const REG_DATA_FN_SEL_PTR: u8 = 0x12;
+    const REG_CONTRACT_ADDR_PTR: u8 = 0x13;
+    const REG_FN_SELECTOR_PTR: u8 = 0x14;
+    const REG_MSG_AMOUNT: u8 = 0x15;
+
+    //referenced data start pointer
+    const REF_DATA_START_PTR: u16 = 11 * BYTES_PER_INSTR;
 
     /* The following assembly code is intended to do the following:
      *  -Call the function `process_message` on the contract with ID that matches
      *   the first 32 bytes in the message data field, while forwarding the exact
      *   amount of base asset specified in the `InputMessage` `amount` field
+     *
+     * note: this code makes the assumption that all memory at VM initialization is set to 0
      */
     let mut script: Vec<u8> = vec![
         //extend stack for contract call data
-        op::move_(MEMORY_START_PTR, STACK_PTR), //MEMORY_START_PTR = stack pointer
+        op::move_(REG_MEMORY_START_PTR, RegId::SP), //REG_MEMORY_START_PTR = stack pointer
         op::cfei(32 + 32 + 8 + 8), //extends current call frame stack by 32+32+8+8 bytes [base asset id, contract id, param1, param2]
-        op::addi(CALL_DATA_PTR, MEMORY_START_PTR, 32), //CALL_DATA_PTR = MEMORY_START_PTR + 32bytes [memory start pointer + 32]
-        op::addi(CALL_DATA_FN_SEL_PTR, CALL_DATA_PTR, 32 + 4), //CALL_DATA_FN_SEL_PTR = CALL_DATA_PTR + 32bytes + 4bytes [call data start pointer + 32 + 4]
+        op::addi(REG_DATA_PTR, REG_MEMORY_START_PTR, 32), //REG_DATA_PTR = REG_MEMORY_START_PTR + 32bytes [memory start pointer + 32]
+        op::addi(REG_DATA_FN_SEL_PTR, REG_DATA_PTR, 32 + 4), //REG_DATA_FN_SEL_PTR = REG_DATA_PTR + 32bytes + 4bytes [call data start pointer + 32 + 4]
         //prep call parameters
-        op::gtf(MSG_AMOUNT, ZERO, GTF_MSG_AMOUNT), //MSG_AMOUNT = amount value of message from input[0]
-        op::gtf(CONTRACT_ADDR_PTR, ZERO, GTF_MSG_DATA), //CONTRACT_ADDR_PTR = memory location of the message data from input[0]
-        op::addi(FN_SELECTOR_PTR, INSTR_START, 11 * BYTES_PER_INSTR), //FN_SELECTOR_PTR = function selector at end of program
-        op::mcpi(CALL_DATA_PTR, CONTRACT_ADDR_PTR, 32), //32 bytes at CALL_DATA_PTR = the 32 bytes at CONTRACT_ADDR_PTR
-        op::mcpi(CALL_DATA_FN_SEL_PTR, FN_SELECTOR_PTR, 4), //4 bytes at CALL_DATA_FN_SEL_PTR = the 4 bytes at FN_SELECTOR_PTR
+        op::gtf(REG_MSG_AMOUNT, RegId::ZERO, GTF_REG_MSG_AMOUNT), //REG_MSG_AMOUNT = amount value of message from input[0]
+        op::gtf(REG_CONTRACT_ADDR_PTR, RegId::ZERO, GTF_MSG_DATA), //REG_CONTRACT_ADDR_PTR = memory location of the message data from input[0]
+        op::addi(REG_FN_SELECTOR_PTR, RegId::IS, REF_DATA_START_PTR), //REG_FN_SELECTOR_PTR = function selector at end of program
+        op::mcpi(REG_DATA_PTR, REG_CONTRACT_ADDR_PTR, 32), //32 bytes at REG_DATA_PTR = the 32 bytes at REG_CONTRACT_ADDR_PTR
+        op::mcpi(REG_DATA_FN_SEL_PTR, REG_FN_SELECTOR_PTR, 4), //4 bytes at REG_DATA_FN_SEL_PTR = the 4 bytes at REG_FN_SELECTOR_PTR
         //make contract call
-        op::call(CALL_DATA_PTR, MSG_AMOUNT, ASSET_ID_PTR, CGAS),
-        op::ret(ZERO),
+        op::call(REG_DATA_PTR, REG_MSG_AMOUNT, REG_ASSET_PTR, RegId::CGAS),
+        op::ret(RegId::ZERO),
         //referenced data (function selector)
         //00000000
     ]
